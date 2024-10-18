300cad439cb7ff82907beef14ecf5ba1d83854e0
gabriel-trinidad-ethicscode <gtrinidad@ethicscode.ai>
2024-10-18 16:00:07 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  data() {
+    return {
+      showChild: true,
+      message: 'Waiting for event...',
+    };
+  },
+  methods: {
+    handleEvent(payload) {
+      this.message = payload;
+    },
+    toggleChild() {
+      this.showChild = !this.showChild;
+    },
+  },
+components: {
+    HelloWorld,
+  },
+};
+</script>
+<template>
+    <div>
+      <HelloWorld v-if="showChild" @childEvent="handleEvent" />
+      <button @click="toggleChild">Toggle Child</button>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Child Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+      this.$emit('childEvent', 'Event emitted after 3 seconds');
+    }, 3000);
+  },
+};
+</script>
